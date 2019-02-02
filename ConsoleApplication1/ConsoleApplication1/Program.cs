f6dfda62e7848966f0d2c29f95a041568b071ed9
hasan0244 <hasan0244@abv.bg>
2019-02-02 13:21:19 +0200

    dsrfds Signed-off-by: hasan0244 <hasan0244@abv.bg> sdfas

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var d = n * 100;
+            var f = d * 365;
+            var k = f * 24;
+            var t = k * 60;
+            Console.WriteLine(d);
+            Console.WriteLine(f);
+            Console.WriteLine(k);
+            Console.WriteLine(t);
+        }
+    }
+}
