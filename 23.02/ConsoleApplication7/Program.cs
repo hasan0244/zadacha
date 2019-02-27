070cc6f4e839eeb89251ad2252fe8d99c554186f
hasan0244 <hasan0244@abv.bg>
2019-02-27 12:10:07 +0200

    fghhf
    Signed-off-by: hasan0244 <hasan0244@abv.bg>

--- 23.02/ConsoleApplication7/Program.cs
+++ 23.02/ConsoleApplication7/Program.cs
@@ -9,6 +9,14 @@ namespace ConsoleApplication7
     {
         static void Main(string[] args)
         {
+            string str = "jdfjkjsd";
+            StringBuilder sb = new StringBuilder();
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                sb.Append(str[i]);
+            }
+            Console.WriteLine( sb);
+
         }
     }
 }
